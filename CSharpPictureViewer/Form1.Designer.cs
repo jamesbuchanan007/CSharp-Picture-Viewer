95d86d7748a003f2a6c649ea90a3ed5ba4283a6c
jamesbuchanan007 <jamesbuchanan007@gmail.com>
2016-05-01 12:15:09 -0500

    Worked on Load, Back, and Forward functions

---
+++ CSharpPictureViewer/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace CSharpPictureViewer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoadPics = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnForward = new System.Windows.Forms.Button();
+            this.btnShuffle = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::CSharpPictureViewer.Properties.Resources.amazing_coral_reefs_12;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(508, 298);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoadPics
+            // 
+            this.btnLoadPics.BackColor = System.Drawing.Color.Green;
+            this.btnLoadPics.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoadPics.Font = new System.Drawing.Font("Andy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadPics.Location = new System.Drawing.Point(12, 319);
+            this.btnLoadPics.Name = "btnLoadPics";
+            this.btnLoadPics.Size = new System.Drawing.Size(75, 25);
+            this.btnLoadPics.TabIndex = 1;
+            this.btnLoadPics.Text = "Load Pics";
+            this.toolTip1.SetToolTip(this.btnLoadPics, "Start Here!!");
+            this.btnLoadPics.UseVisualStyleBackColor = false;
+            this.btnLoadPics.Click += new System.EventHandler(this.btnLoadPics_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Green;
+            this.btnBack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnBack.Font = new System.Drawing.Font("Andy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(188, 318);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 25);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnForward
+            // 
+            this.btnForward.BackColor = System.Drawing.Color.Green;
+            this.btnForward.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnForward.Font = new System.Drawing.Font("Andy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForward.Location = new System.Drawing.Point(269, 318);
+            this.btnForward.Name = "btnForward";
+            this.btnForward.Size = new System.Drawing.Size(75, 25);
+            this.btnForward.TabIndex = 3;
+            this.btnForward.Text = "Forward";
+            this.btnForward.UseVisualStyleBackColor = false;
+            this.btnForward.Click += new System.EventHandler(this.btnForward_Click);
+            // 
+            // btnShuffle
+            // 
+            this.btnShuffle.BackColor = System.Drawing.Color.Green;
+            this.btnShuffle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnShuffle.Font = new System.Drawing.Font("Andy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShuffle.Location = new System.Drawing.Point(446, 318);
+            this.btnShuffle.Name = "btnShuffle";
+            this.btnShuffle.Size = new System.Drawing.Size(75, 26);
+            this.btnShuffle.TabIndex = 4;
+            this.btnShuffle.Text = "AutoPlay";
+            this.toolTip1.SetToolTip(this.btnShuffle, "Random Shuffler");
+            this.btnShuffle.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Font = new System.Drawing.Font("Andy", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(446, 370);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 25);
+            this.btnExit.TabIndex = 5;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 407);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnShuffle);
+            this.Controls.Add(this.btnForward);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnLoadPics);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Andy", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Picture Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoadPics;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnForward;
+        private System.Windows.Forms.Button btnShuffle;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ CSharpPictureViewer/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpPictureViewer
+{
+    public partial class frmMain : Form
+    {
+        protected string[] picFileName;
+        protected int picCurrentImage = -1;
+        protected bool youAreCool = true;
+        
+        public frmMain()
+        {
+            InitializeComponent();            
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        public void btnLoadPics_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "JPEG|*.jpg|Bitmaps|*.bmp";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                picFileName = ofd.FileNames;
+                picCurrentImage = 0;
+                pictureBox1.BackgroundImage = null;
+                pictureBox1.Image = Bitmap.FromFile(picFileName[picCurrentImage]);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (youAreCool)
+            {
+                picCurrentImage = picCurrentImage == 0 ? picFileName.Length - 1 : --picCurrentImage;
+                pictureBox1.Image = Bitmap.FromFile(picFileName[picCurrentImage]);
+            }
+        }
+
+        private void btnForward_Click(object sender, EventArgs e)
+        {
+            if (youAreCool)
+            {
+                picCurrentImage = picCurrentImage == picFileName.Length - 1 ? 0 : ++picCurrentImage;
+                pictureBox1.Image = Bitmap.FromFile(picFileName[picCurrentImage]);
+            }
+        }
+    }
+}
